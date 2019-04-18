342da063b9f78dcde7d969776ad753299fa4cdf8
Daichi Kimura <daichi703n@gmail.com>
2019-04-18 19:37:19 +0900

    Add pivotal tracker labels

--- scripts/generate-pivotal-tracker-story.sh
+++ scripts/generate-pivotal-tracker-story.sh
@@ -1,16 +1,18 @@
 create_story_payload() {
   name="$1 upgrade $2"
   description="This is an auto generated story created for $1 upgrade to $2"
-  echo '{"name":"'"$name"'","description":"'"$description"'"}'
+  labels=$(echo $3 | sed -e 's/\[/\"/g' | sed -e 's/,/\",\"/g' | sed -e 's/\]/\"/g')
+  echo '{"name":"'"$name"'","description":"'"$description"'","labels":['"$labels"']}'
 }
 
 generate_pivtal_tracker_story() {
   local component=$1
   local tag=$2
+  local labels=$3
 
   # See https://www.pivotaltracker.com/help/api/rest/v5#Stories
   token=${PIVOTAL_TRACKER_TOKEN}
   project_id=${PIVOTAL_TRACKER_PROJECT_ID}
-  payload=$(create_story_payload "$component" "$tag")
+  payload=$(create_story_payload "$component" "$tag" "$labels")
   curl -X POST -H "X-TrackerToken: ${token}" -H "Content-Type: application/json" -d "$payload" "https://www.pivotaltracker.com/services/v5/projects/${project_id}/stories"
 }
